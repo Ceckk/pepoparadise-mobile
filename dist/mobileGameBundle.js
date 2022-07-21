7fd50469d4d3b06fa228e17d1780a47190d2c892
Ceck <andrea.cecovig@gmail.com>
2022-07-21 17:34:16 +0200

    * ios universal link configuration

--- dist/mobileGameBundle.js
+++ dist/mobileGameBundle.js
@@ -33310,12 +33310,56 @@ async function signMessage(message)
         await new Promise((resolve) => setTimeout(resolve, 1000));
         const signature = await signer.signMessage(message);
         console.log({ signature });
-        window.location.replace("pepodl://sign?"+signature);
+
+        const os = getMobileOperatingSystem();
+        if (os == "Android")
+        {
+            window.location.replace("pepodl://sign?" + signature);
+        }
+        else if (os == "iOS")
+        {
+            window.location.replace(window.location.href + "pepodl?" + signature);
+        }
+        else
+        {
+            console.log(os + " platform not supported");
+        }
     }
     catch (error)
     {
+        console.log(error);
         console.log("Signature Denied");
     }
+}
+
+/**
+ * Determine the mobile operating system.
+ * This function returns one of 'iOS', 'Android', 'Windows Phone', or 'unknown'.
+ *
+ * @returns {String}
+ */
+function getMobileOperatingSystem()
+{
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent))
+    {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent))
+    {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream)
+    {
+        return "iOS";
+    }
+
+    return "unknown";
 }
 
 },{"ethers":146}]},{},[168]);
